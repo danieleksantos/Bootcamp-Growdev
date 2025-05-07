0d2c661695245e8ef7cc41891170bf95eb9ceb21
Daniele Karina dos Santos <danimistron@gmail.com>
2025-05-07 12:27:53 -0300

    consumo api ok

---
+++ prototipo-1/js/api.js
@@ -0,0 +1,42 @@
+const api = axios.create({
+    baseURL: 'https://rickandmortyapi.com/api/'
+});
+
+//total de uma funcionalidade
+async function getTotalByFeature(feature) {
+
+    try {
+        const result = await api.get(`/${feature}`);
+
+        return result.data.info.count;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//listagem dos personagens de acordo com a pagina selecionada
+async function listCharacterByPage(page = 1) {
+    try {
+        const result = await api.get("/character", {
+            params: { page },
+        });
+
+        return {
+            nextPage: result.data.info.next,
+            prevPage: result.data.info.prev,
+            charactersList: result.data.results,
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//buscar nome de episodio usando URL
+async function getEpisodeDataFromURL(url) {
+    try {
+        const result = await api.get(url);
+        return result.data.name
+    } catch (error) {
+        console.log(error)
+    }
+}
